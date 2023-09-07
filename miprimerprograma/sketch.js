0327add8f4c8b8ce558f7e23e98ce405b957027e
Xxmataburros545xX <144397507+Xxmataburros545xX@users.noreply.github.com>
2023-09-07 17:47:25 -0600

    Add files via upload

---
+++ miprimerprograma/sketch.js
@@ -0,0 +1,6 @@
+var nombre = "alberto "
+var deportes = "natacion y basketball, "
+
+
+var edad = 13 ;
+console.log("hola, soy "+ nombre + "mis deportes favoritos son "+ deportes + "y tengo " + edad + "años " ); 
